ac7ab92cfe4b43d351b45aed1f18a8a68a46d9b3
Keith Hering <heringks@outlook.com>
2021-11-03 23:41:20 -0700

    Updated Intersection.cpp per mentor feedback

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -71,6 +71,7 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
     std::promise<void> prms;
     std::future<void> ftr = prms.get_future();
     _waitingVehicles.pushBack(vehicle, std::move(prms));
+    ftr.wait();
 
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
 }
